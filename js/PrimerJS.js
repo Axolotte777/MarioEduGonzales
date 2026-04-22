f6add43b35a92c9c21257f667e4c9e6378dc1d89
MarioEdu <gmarioedu1@gmail.com>
2026-04-22 16:03:48 -0500

    .

--- js/PrimerJS.js
+++ js/PrimerJS.js
@@ -6,14 +6,12 @@ alert("Virus incoming");
 function cambiarCarita(){
     document.getElementById("carita").src="images/sad.jpg";
     alert("Uma triste...??");
-    
-    
+
 }
 function feliz(){
     document.getElementById("carita").src="images/happy.jpg";
     document.getElementById("TituloJS").textContent="Ejemplo de emoción";
 }
-
 function nombresNav() {
     document.getElementById("m1").textContent = "Hogar";
     document.getElementById("m2").textContent = "Listas";
@@ -22,4 +20,16 @@ function nombresNav() {
     document.getElementById("m5").textContent = "Js tarea";
     document.getElementById("m6").textContent = "quinta pagina";
     document.getElementById("m7").textContent = "Personal";
+}
+function cambiarTitulo(){
+    document.getElementById("titulo").textContent="Practicando JS";
+    document.getElementById("titulo").style.color="#5a4581" 
+    document.getElementById("logoSenati").src="images/senati3.png";
+    document.getElementById("encabezado").style.background="#b399d1"
+}
+function suma(){
+    let num1 = 6;
+    let num2 = 7;
+    let suma = num1+num2;
+    alert("la suma es de" +suma);
 }
