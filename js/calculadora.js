2a69e8d726ca6eac6f22e2b5630a6193db9a9f07
MarioEdu <gmarioedu1@gmail.com>
2026-04-23 16:09:46 -0500

    .

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,17 +1,39 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    document.getElementById("formCalculadora").addEventListener("submit", function(e)){
+    document.getElementById("formCalculadora").addEventListener("submit", function(e){
         e.preventDefault();// evita que el formulario me vote de la pagina
 
-        let mume1 = parseFloat(document.getElementById("numero1").value);
-        let mume2 = parseFloat(document.getElementById("numero2").value);
+        let nume1 = parseFloat(document.getElementById("numero1").value);
+        let nume2 = parseFloat(document.getElementById("numero2").value);
         let operacion = document.getElementById("operacion").value;
         let resultado = document.getElementById("resultado");
 
         if(isNaN(nume1)||isNaN(nume2)){
             resultado.textContent = "Ingrese números válidos";
-        
+            return;
         }
-    }
+
+        let res; //temporal para almacenar el resultado
+
+        switch (operacion) {
+            case "sum":
+                res = nume1+nume2;
+                break;
+            case "rest":
+                res = nume1-nume2;
+                break;
+            case "mul":
+                res = nume1*nume2;
+                break;
+            case "div":
+
+                break;
+            default:
+                break;
+        }
+
+    resultado.textContent = "Resultado es "+res;
+
+    });
 
 })
