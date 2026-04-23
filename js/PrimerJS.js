b3a1f14ea31dd2a9fe7fd245869831aaffc8ee9a
MarioEdu <gmarioedu1@gmail.com>
2026-04-23 15:18:41 -0500

    .

--- js/PrimerJS.js
+++ js/PrimerJS.js
@@ -79,6 +79,6 @@ function dosD(){
 }
 function tresD(){
     document.getElementById("2d").src="images/3d.jpg";
-    document.getElementById("2D Y 3D").textContent="Mc y Trra";
+    document.getElementById("2D y 3D").textContent="Mc y Trr";
     alert("3D Minecraft");
 }
